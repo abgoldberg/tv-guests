77bf6f00cf2e69694f2d5c0c5a80f88b84f1bd1b
Andrew B. Goldberg <andrew@arcode.com>
2013-12-14 19:33:17 -0500

    Generalize the script that augments scrapy json output with dbpedia predicate-object pairs

--- README.md
+++ README.md
@@ -16,5 +16,5 @@ Here's an example of how to crawl wikipedia for episode data.
 
 Possible spider names are: tds, colbert, fallon
 
-    cd scrapy_tutorial
+    cd scrapy_project
     scrapy crawl name -o name_items.json -t json
--- analyze_json.py
+++ analyze_json.py
@@ -2,14 +2,23 @@ import json
 import dbpedia
 import rdflib
 import time
+import sys
 
-input_file = 'scrapy_tds/items.json'
+#input_file = 'scrapy_tds/items.json'
+input_file = sys.argv[1]
+
 data = json.load(open(input_file))
+
+print "Loaded %s episodes of data from %s" % (len(data), input_file)
 
 output_file = '%s.extended' % (input_file)
 
+print "Will write extended json to %s" % (output_file)
+
+raw_input("Press Enter to continue...")
+
 for idx,episode in enumerate(data):
-    print "Processing episode %s" % (episode)
+    print "Processing episode\n\t%s" % (episode)
 
     episode['guest_predicate_objects'] = {}
     for resource in episode['guest_resources']:
@@ -20,6 +29,9 @@ for idx,episode in enumerate(data):
         p_o = episode['guest_predicate_objects'][resource] = []
         for (p, o) in g.predicate_objects(res):
             p_o.append((p,o))
+
+        print "Augmented episode with %s predicate-object pairs about %s" % (len(p_o), resource)
+    print ""
 
     time.sleep(0.2) # don't hammer too hard
 
